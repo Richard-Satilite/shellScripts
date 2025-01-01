ed95e5c22c1ae7bbca64da70df99975bbdcd7649
Richard <richardsatilite.dev@gmail.com>
2025-01-01 15:07:43 -0300

    BS: Backup Script now can test backup PATH

--- backupScript.sh
+++ backupScript.sh
@@ -2,11 +2,18 @@
 
 # SCRIPT PARA REALIZAÇÃO DE BACKUPS DE ARQUIVOS | SCRIPT FOR PERFORMING FILE BACKUP
 
-backup_dir = "dir_PATH"
+backup_dir="backupFrom_PATH"
 
+if [ -e backup_dir ]; then
 # backup name
-backup_name = "backup_$(date +%Y%m%d_%H%M%S).tar.gz"
+backup_name="backup_$(date +%Y%m%d_%H%M%S).tar.gz"
 
 tar -czf "$file_name" "backup_dir"
 
 echo "Successful backup in $backup_name"
+
+else
+
+echo "Path '$backup_dir' doesn't exist"
+
+fi
